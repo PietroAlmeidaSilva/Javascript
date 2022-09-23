81650216ad9d95377625d24c734bf926e2c9d713
Pietro Almeida Alves Silva <110930869+PietroAlmeidaSilva@users.noreply.github.com>
2022-09-23 15:35:55 -0300

    atualizado

---
+++ aula11/ex08.js
@@ -0,0 +1,7 @@
+console.log('Verificando sua velocidade.')
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}!`)
+if (vel > 60){ //Condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre com o cinto de segurança!')
---
+++ aula11/ex09.js
@@ -0,0 +1,8 @@
+var nacio = 'Brasil'
+console.log('Olá, de onde você veio?')
+if (nacio == 'Brasil') {
+    console.log('Você é Brasileiro.')
+} else {
+    console.log('Você é Estrangeiro.')
+}
+console.log('Obrigado bem-vindo(a) ao Brasil.')
