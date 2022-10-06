28bda11e455ce98b7f189c396801954865a2bdbf
Pietro Almeida Alves Silva <110930869+PietroAlmeidaSilva@users.noreply.github.com>
2022-10-06 16:09:40 -0300

    atualizado

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var passo = 1
+/*
+while (passo <= 100) {
+    console.log(`Passo ${passo}`)
+    passo++
+}
+
+/*
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+*/
